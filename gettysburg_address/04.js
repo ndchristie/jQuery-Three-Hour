5f5fca539998b6ddd9eed257afd44f76cbdc63bf
Nathan Christie <ndchristie@gmail.com>
2015-01-29 21:00:49 -0800

    gettysburg assignment into .js file

---
+++ gettysburg_address/04.js
@@ -0,0 +1,10 @@
+/* 04.js */
+
+//Ass.:Download the exercise files here.
+//Done
+
+//Ass.:Alter the stylesheet to hide the contents of the page initially. When the page is loaded, fade in the contents slowly.
+//Ass.:Give each paragraph a yellow background only when the mouse is over it.
+//Ass.:Make a click of the title (<h2>) and simultaneously fade it to 25 percent opacity and grow its left-hand margin to 20px. Then, when this animation is complete, fade the speech text to 50 percent opacity.
+//Ass.:Make the style switcher work.
+//Ass.:Challenge: React to presses of the arrow keys by smoothly moving the switcher box 20 pixels in the corresponding direction. The key codes for the arrow keys are: 37 (left), 38 (up), 39 (right), and 40 (down).
