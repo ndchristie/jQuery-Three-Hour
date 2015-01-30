e88318f2237ebe284dd7124e1168da49aabc2213
Nathan Christie <ndchristie@gmail.com>
2015-01-29 19:26:27 -0800

    Add a class of year to all of the table cells in the third column of a table.

--- shakespeares_plays/02.js
+++ shakespeares_plays/02.js
@@ -10,6 +10,9 @@ $('#selected-plays > li > ul > li').addClass('special');
 
 //Ass.:Add a class of year to all of the table cells in the third column of a table.
 //Ass.:Hint: Take a look at how many columns are in each table.
+$('td:nth-of-type(3)').addClass('year');
+//Done
+
 //Ass.:Add the class of special to the first table row that has the word Tragedy in it.
 //Ass.:Challenge: Select all of the list items (<li>) containing a link (<a>). Add the class afterlink to the sibling list items that follow the ones selected.
 //Ass.:Challenge: Add the class tragedy to the closest ancestor (<ul>) of any .pdf link.
