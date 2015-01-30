21feede530cd840e1d1f37899dd4d158556815a5
Nathan Christie <ndchristie@gmail.com>
2015-01-29 20:14:49 -0800

    adding annotations

--- shakespeares_plays/02.js
+++ shakespeares_plays/02.js
@@ -19,8 +19,18 @@ $('tr:contains("Tragedy"):first').addClass('special');
 
 //Ass.:Challenge: Select all of the list items (<li>) containing a link (<a>). Add the class afterlink to the sibling list items that follow the ones selected.
 $('a').closest('li').nextAll('li').addClass('afterlink');
+/*
+solution given is $('li:has(a)').nextAll().addClass('afterlink');
+this is the equivalent of $('li:has(a) ~ *').addClass('afterlink');
+both of these apply a green border to the first-level lists as well as the siblings of the anchor-containing item.
+both of these will apply to any sibling elements; supplying "li" will make explicit.
+*/
 //Done
 
 //Ass.:Challenge: Add the class tragedy to the closest ancestor (<ul>) of any .pdf link.
 $('a[href*=".pdf"]').closest('ul').addClass('tragedy');
+/*
+an alternative solution given is $('a[href*=".pdf"]').parents('ul:first').addClass('tragedy');
+this is not equivalent, insofar as it only returns the first in the tree, not the closest to the anchor.
+*/
 //Done
