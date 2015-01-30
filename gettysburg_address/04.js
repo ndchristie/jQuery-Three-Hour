ac098f6628d39bbb24379ebc784279d2384e5102
Nathan Christie <ndchristie@gmail.com>
2015-01-29 21:09:27 -0800

    Make a click of the title (<h2>) and simultaneously fade it to 25 percent opacity and grow its left-hand margin to 20px.
    Then, when this animation is complete, fade the speech text to 50
    percent opacity.

--- gettysburg_address/04.js
+++ gettysburg_address/04.js
@@ -18,5 +18,17 @@ $(document).on('mouseenter', 'p', function(){
 //Done
 
 //Ass.:Make a click of the title (<h2>) and simultaneously fade it to 25 percent opacity and grow its left-hand margin to 20px. Then, when this animation is complete, fade the speech text to 50 percent opacity.
+$(document).on('click', 'h2', function(){
+	$(this).animate({
+		'opacity':0.25,
+		'left':'20px',
+	}, 1000, function(){
+		$('.speech p').animate({
+			'opacity':0.5,
+		});
+	});
+});
+//Done
+
 //Ass.:Make the style switcher work.
 //Ass.:Challenge: React to presses of the arrow keys by smoothly moving the switcher box 20 pixels in the corresponding direction. The key codes for the arrow keys are: 37 (left), 38 (up), 39 (right), and 40 (down).
