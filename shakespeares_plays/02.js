bf258d823187beb75f9e027611399c4f285f5a8f
Nathan Christie <ndchristie@gmail.com>
2015-01-29 19:15:14 -0800

    initial commit - assignment into js file

---
+++ shakespeares_plays/02.js
@@ -0,0 +1,12 @@
+/* 02.js */
+
+//Ass.:Download the starter code files here.
+//console.log($);
+//Done
+
+//Ass.:Add a class of special to all of the <li> elements at the second level of the nested list.
+//Ass.:Add a class of year to all of the table cells in the third column of a table.
+//Ass.:Hint: Take a look at how many columns are in each table.
+//Ass.:Add the class of special to the first table row that has the word Tragedy in it.
+//Ass.:Challenge: Select all of the list items (<li>) containing a link (<a>). Add the class afterlink to the sibling list items that follow the ones selected.
+//Ass.:Challenge: Add the class tragedy to the closest ancestor (<ul>) of any .pdf link.
